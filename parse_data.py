4c9d2734b9172cbad425500d0566a3384f6d6cc3
Alex McNurlin <alemcnurlin@gmail.com>
2019-03-30 13:11:55 -0700

    Add metacritic histogram

--- parse_data.py
+++ parse_data.py
@@ -14,14 +14,16 @@ fields = [
 ]
 
 # for field in fields:
-# 		df1 = df[field][getattr(df, field) > 2]
-# 		ax = df1.plot.hist(by=field, bins=75)
-# 		ax.plot()
-# 		plt.savefig("hist_{}.png".format(field))
+#     df1 = df[field][getattr(df, field) > 2]
+#     ax = df1.plot.hist(by=field, bins=75)
+#     ax.plot()
+#     plt.savefig("hist_{}.png".format(field))
 
-df2 = df["Metacritic"][df.Metacritic > 2]
-ax = df2.plot.hist(by="ReleaseYear", bins=75)
+df2 = df["Metacritic"][df.Metacritic > 1]
+ax = df2.plot.hist(by="Metacritic", bins=75)
+ax.set_xlabel("Metacritic score")
+ax.set_title("Metacritic score distribution")
 ax.plot()
-plt.savefig("test.png")
+plt.savefig("hist_Metacritic.png")
 
 # print(df.loc[0])
