464350c00bca6ac2eaf60eb1e725aef1b0d1e456
Alex McNurlin <alemcnurlin@gmail.com>
2019-03-30 11:28:07 -0700

    Add beginning python script

---
+++ parse_data.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("games-features.csv")
+
+fields = [
+    "Metacritic",
+    "ReleaseDate"
+]
+
+# for field in fields:
+# 		df1 = df[field][getattr(df, field) > 2]
+# 		ax = df1.plot.hist(by=field, bins=75)
+# 		ax.plot()
+# 		plt.savefig("hist_{}.png".format(field))
+
+df2 = df["Metacritic"][df.Metacritic > 2]
+ax = df2.plot.hist(by="ReleaseYear", bins=75)
+ax.plot()
+plt.savefig("test.png")
+
+# print(df.loc[0])
